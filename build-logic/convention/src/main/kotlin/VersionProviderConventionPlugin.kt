9c6d618fc2d8964e31ad86b0456e04c80c987c4d
Juan Gomez <juan.e.g.f@outlook.com>
2023-12-15 10:10:01 -0600

    feat: Fix plugin error by mapping provider

--- build-logic/convention/src/main/kotlin/VersionProviderConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/VersionProviderConventionPlugin.kt
@@ -1,11 +1,10 @@
-import com.android.build.api.artifact.SingleArtifact
 import com.android.build.api.variant.ApplicationAndroidComponentsExtension
+import com.android.build.api.variant.VariantOutputConfiguration.OutputType
 import com.android.build.gradle.internal.plugins.AppPlugin
 import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.RegularFileProperty
-import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import java.io.File
@@ -16,33 +15,36 @@ class VersionProviderConventionPlugin : Plugin<Project> {
             val applicationExtension = project.extensions
                 .getByType(ApplicationAndroidComponentsExtension::class.java)
 
-            val release = applicationExtension.selector().withBuildType("release")
+            with(applicationExtension) {
+                val release = selector().withBuildType("release")
 
-            applicationExtension
-                .onVariants(release) { variant ->
-                    println("Current variant is ${variant.name}")
+                applicationExtension
+                    .onVariants(release) { variant ->
+                        println("Current variant is ${variant.name}")
+                        // Because an app module can have multiple outputs when using multi-APK, versionCode
+                        // is only available on the variant output.
+                        // Gather the output when we are in single mode and there is no multi-APK.
+                        val mainOutput = variant.outputs.single { it.outputType == OutputType.SINGLE }
 
-                    // Register single task for getting versions from git
-                    val gitVersionProvider =
-                        project.tasks.register(variant.name + "GitVersionProvider", GitVersionTask::class.java) {
-                            gitVersionOutputFile.set(
-                                File(project.layout.buildDirectory.get().asFile.absoluteFile, "intermediates/gitVersionProvider/release/output")
-                            )
-                            outputs.upToDateWhen { false } // never use cache
-                        }
+                        // Register single task for getting versions from git
+                        val gitVersionProvider =
+                            project.tasks.register(variant.name + "GitVersionProvider", GitVersionTask::class.java) {
+                                gitVersionOutputFile.set(
+                                    File(project.layout.buildDirectory.get().asFile.absoluteFile, "intermediates/gitVersionProvider/release/output")
+                                )
+                                outputs.upToDateWhen { false } // never use cache
+                            }
 
-                    val manifestUpdater =
-                        project.tasks.register(variant.name + "ManifestUpdater", ManifestTransformerTask::class.java) {
-                            gitInfoFile.set(gitVersionProvider.flatMap(GitVersionTask::gitVersionOutputFile))
-                        }
-                    // update manifest with version information that we got from gitVersionProvider
-                    variant.artifacts.use(manifestUpdater)
-                        .wiredWithFiles(
-                            ManifestTransformerTask::mergedManifest,
-                            ManifestTransformerTask::updatedManifest
+                        mainOutput.versionCode.set(
+                            gitVersionProvider
+                                .flatMap{ task ->
+                                    task.gitVersionOutputFile.map { versionOutput ->
+                                        versionOutput.asFile.readText().toInt()
+                                    }
+                                }
                         )
-                        .toTransform(SingleArtifact.MERGED_MANIFEST)
-                }
+                    }
+            }
         }
 
     }
@@ -75,33 +77,9 @@ internal abstract class GitVersionTask: DefaultTask() {
             stream.bufferedReader().use { buffer ->
                 buffer.lines().count()
             }
-        } + 1
+        }
 
         println("Number of git tags: $version")
         gitVersionOutputFile.get().asFile.writeText(version.toString())
-    }
-}
-
-internal abstract class ManifestTransformerTask: DefaultTask() {
-
-    @get:InputFile
-    abstract val gitInfoFile: RegularFileProperty
-
-    @get:InputFile
-    abstract val mergedManifest: RegularFileProperty
-
-    @get:OutputFile
-    abstract val updatedManifest: RegularFileProperty
-
-    @TaskAction
-    fun taskAction() {
-        val gitVersion = gitInfoFile.get().asFile.readText()
-        var manifest = mergedManifest.asFile.get().readText()
-        manifest = manifest.replaceFirst(
-            regex = "android:versionCode=\"[0-9]+\"".toRegex(),
-            replacement = "android:versionCode=\"$gitVersion\""
-        )
-        println("Writes to " + updatedManifest.get().asFile.absolutePath)
-        updatedManifest.get().asFile.writeText(manifest)
     }
 }
