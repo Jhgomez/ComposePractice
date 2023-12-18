b5849e15ba356c8aaa06717f7cdd0544d6935f70
Juan Gomez <juan.e.g.f@outlook.com>
2023-12-17 20:34:30 -0600

    feat: Add compose jetbrains compose multiplatform plugin and KMP plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
-//list of plugins used in the app
 plugins {
+    //list of plugins used in the app
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
