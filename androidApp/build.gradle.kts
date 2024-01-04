56c69218af065933f8e2c37d4f37a9563849a3aa
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-04 11:10:10 -0600

    feat: add internal dependency

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -2,23 +2,6 @@ plugins {
     alias(libs.plugins.okik.application)
 }
 
-kotlin {
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-
-    sourceSets {
-
-    }
-}
-
 android {
     namespace = "okik.tech.community.admin"
 
@@ -62,6 +45,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    implementation(project(":shared"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
