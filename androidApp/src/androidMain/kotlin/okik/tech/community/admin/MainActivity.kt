65913b01e645d8d0d6eacdf8e7d3c2d8b32cd032
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-06 23:45:58 -0600

    feat: Call composable from activity

--- androidApp/src/androidMain/kotlin/okik/tech/community/admin/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/okik/tech/community/admin/MainActivity.kt
@@ -1,12 +1,17 @@
 package okik.tech.community.admin
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import okik.tech.community.admin.shared.Home
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setContent {
+            Home()
+        }
     }
 }
