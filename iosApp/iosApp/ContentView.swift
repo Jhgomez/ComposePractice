795f5a8cb5833a9b15e3e1e121b1cff181b65fea
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-08 16:26:31 -0600

    feat: Add new ios project

---
+++ iosApp/iosApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iosApp
+//
+//  Created by Juan Enrique Gomez Flores on 8/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
