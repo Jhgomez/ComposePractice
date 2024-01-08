2eaede1c6539bfe736b706ab980a85355f88d875
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-08 12:55:41 -0600

    feat: Add cocoa pods configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     //list of plugins used in the app
+    alias(libs.plugins.kotlin.native.cocoapods) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.compose.multiplatform) apply false
-    alias(libs.plugins.kotlin.native.cocoapods) apply false
 }
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -5,3 +5,16 @@ plugins {
 android {
     namespace = "okik.tech.community.admin.shared"
 }
+
+kotlin {
+    cocoapods {
+        version = "1.0.0"
+        summary = "Some description for the Shared Module"
+        homepage = "Link to the Shared Module homepage"
+        podfile = project.file("../iosapp/Podfile")
+        framework {
+            baseName = "shared"
+            isStatic = true
+        }
+    }
+}
