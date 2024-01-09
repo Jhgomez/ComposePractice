4771c82fb194424219631996d543e27ea15375a4
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-08 23:30:06 -0600

    feat: Start new ios project to configure it the right way through xcode

---
+++ iosApp/iosApp/iosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Juan Enrique Gomez Flores on 8/01/24.
+//
+
+import SwiftUI
+
+@main
+struct iosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
