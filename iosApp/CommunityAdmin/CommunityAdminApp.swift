90d354a477acd5d78a81a581363d11ce42554da8
Juan Gomez <juan.e.g.f@outlook.com>
2024-01-09 09:13:14 -0600

    feat: Add iosApp xcode project again

---
+++ iosApp/CommunityAdmin/CommunityAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityAdminApp.swift
+//  CommunityAdmin
+//
+//  Created by Juan Enrique Gomez Flores on 9/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CommunityAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
