1497ddbf255fcf761a92f9e1dd870f9c115de712
Juan Gomez <juan.e.g.f@outlook.com>
2024-05-11 21:17:06 -0600

    chore: Add demo UI

--- shared/src/commonMain/kotlin/okik/tech/community/admin/shared/Home.common.kt
+++ shared/src/commonMain/kotlin/okik/tech/community/admin/shared/Home.common.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.resources.compose.painterResource
@@ -19,9 +18,15 @@ import okik.tech.community.admin.shared.resources.SharedRes
 @Composable
 fun Home(modifier: Modifier = Modifier) {
     Column(modifier.fillMaxSize()) {
-        val app_name: String = stringResource(SharedRes.strings.test)
-        val greetingText by remember { mutableStateOf("Hello, World! $app_name") }
+        val appName: String = stringResource(SharedRes.strings.test)
+        val greetingText by remember { mutableStateOf("Hello, xxWorld! $appName") }
+
         Text(greetingText)
-        Image(modifier = Modifier.size(180.dp), painter = painterResource(SharedRes.images.myImage), contentDescription = null)
+
+        Image(
+            modifier = Modifier.size(300.dp),
+            painter = painterResource(SharedRes.images.myImage),
+            contentDescription = null
+        )
     }
 }
