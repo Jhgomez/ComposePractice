f083b511760cc87fb3c7355d285c4ac4c29ca7f8
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-04 17:10:03 -0600

    chore: Add scroll to androi app

--- androidApp/src/androidMain/kotlin/okik/tech/community/admin/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/okik/tech/community/admin/MainActivity.kt
@@ -3,8 +3,11 @@ package okik.tech.community.admin
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import okik.tech.community.admin.shared.Home
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
             Home(modifier = Modifier
                 .fillMaxSize()
                 .padding(16.dp)
+                .verticalScroll(rememberScrollState())
             )
         }
     }
