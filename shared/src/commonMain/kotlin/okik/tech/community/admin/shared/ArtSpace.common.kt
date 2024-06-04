5abac3809dbadb38bfd849df3f626cb8c8939857
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-03 21:42:11 -0600

    chore: add image card composable

--- shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
+++ shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
@@ -1,2 +1,39 @@
 package okik.tech.community.admin.shared
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import dev.icerock.moko.resources.ImageResource
+import dev.icerock.moko.resources.compose.painterResource
+import okik.tech.community.admin.shared.resources.SharedRes
+
+@Composable
+fun ArtSpace() {
+
+}
+
+@Composable
+fun ImageCard(
+    image: ImageResource,
+    modifier: Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 16.dp
+        )
+    ) {
+        Image(
+            painter = painterResource(image),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+    }
+}
