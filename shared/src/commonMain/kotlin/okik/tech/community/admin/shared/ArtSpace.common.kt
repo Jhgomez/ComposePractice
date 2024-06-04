aaf4270702c13f5790cd6e8c364c2a53d4cdc3d2
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-04 14:36:30 -0600

    chore: Add tool tip

--- shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
+++ shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
@@ -14,9 +14,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -75,6 +80,7 @@ fun ColumnScope.ImageCard(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ColumnScope.PictureFooter(
     title: String,
@@ -95,13 +101,23 @@ fun ColumnScope.PictureFooter(
             modifier = Modifier
                 .padding(horizontal = 8.dp, vertical = 24.dp)
         ) {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.fillMaxWidth()
-            )
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberRichTooltipPositionProvider(),
+                tooltip = {
+                    PlainTooltip {
+                        Text(title)
+                    }
+                },
+                state = rememberTooltipState()
+            ) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
 
             val annotatedString = buildAnnotatedString {
                 withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
