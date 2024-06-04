7abcb79e0e67c9cd6ff7aaee3900eb9507f6e8a3
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-03 23:42:13 -0600

    chore: Add first version of art space

--- shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
+++ shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
@@ -43,6 +45,14 @@ fun ArtSpace(modifier: Modifier) {
             image = SharedRes.images.android_logo,
             modifier = Modifier
                 .weight(weight = 0.6f, fill = true)
+        )
+
+        PictureFooter(
+            "My first picture with my new born ",
+            "Dalia Flores",
+            "2009",
+            {},
+            {}
         )
     }
 }
@@ -112,8 +122,30 @@ fun ColumnScope.PictureFooter(
             )
         }
 
-        Row(modifier = Modifier.fillMaxSize()) {
+        Row(
+            modifier = Modifier
+                .weight(.15f, true)
+                .fillMaxWidth()
+        ) {
+            Button(
+                onClick = onPrevious,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Previous"
+                )
+            }
 
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Button(
+                onClick = onNext,
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Next"
+                )
+            }
         }
     }
 }
