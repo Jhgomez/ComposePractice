44ed8dc4c1d252a372320a1852491a06d25fc243
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-04 13:45:29 -0600

    chore: Add padding to ios app

--- shared/src/iosMain/kotlin/okik/tech/community/admin/shared/main.ios.kt
+++ shared/src/iosMain/kotlin/okik/tech/community/admin/shared/main.ios.kt
@@ -1,10 +1,17 @@
 package okik.tech.community.admin.shared
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 
 @Suppress("FunctionName", "unused")
 fun MainViewController(): UIViewController =
-    ComposeUIViewController { Home(modifier = Modifier.fillMaxSize()) }
+    ComposeUIViewController {
+        Home(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+        )
+    }
