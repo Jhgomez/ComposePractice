57ead470d939deaba5d215b84ba6aa55e2354768
Juan Gomez <juan.e.g.f@outlook.com>
2024-06-03 21:42:27 -0600

    chore: Add radius parameter

--- shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
+++ shared/src/commonMain/kotlin/okik/tech/community/admin/shared/ArtSpace.common.kt
@@ -2,12 +2,17 @@ package okik.tech.community.admin.shared
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.resources.ImageResource
 import dev.icerock.moko.resources.compose.painterResource
@@ -21,12 +26,15 @@ fun ArtSpace() {
 @Composable
 fun ImageCard(
     image: ImageResource,
+    cornerRadius: Dp = 0.dp,
     modifier: Modifier
 ) {
     Card(
         elevation = CardDefaults.cardElevation(
             defaultElevation = 16.dp
-        )
+        ),
+        shape = RoundedCornerShape(cornerRadius),
+        modifier = modifier
     ) {
         Image(
             painter = painterResource(image),
